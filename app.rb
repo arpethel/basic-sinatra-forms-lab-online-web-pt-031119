fd90b7ce6b99a87be5344355f2a8803ee1196722
Ryan Pethel <arpethel@gmail.com>
2019-06-16 13:28:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,11 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  get '/team' do
+    # Write your code here!
+    @team = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :team
+  end
+
 
 end
