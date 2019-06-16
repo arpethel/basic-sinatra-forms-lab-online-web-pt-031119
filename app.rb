f99d185cbd01d4a3e0d4f6cdfed1396fea233c59
Ryan Pethel <arpethel@gmail.com>
2019-06-16 14:30:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,12 +15,4 @@ class App < Sinatra::Base
     @team = Team.new(params[:name], params[:coach], params[:pg], params[:sg], params[:pf], params[:sf], params[:c])
     erb :team
   end
-
-  # get '/team' do
-  #   # Write your code here!
-  #   @team = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-  #   erb :team
-  # end
-
-
 end
