88a484aba0483a1ecc2d247339c2701737e503f6
Ryan Pethel <arpethel@gmail.com>
2019-06-16 13:38:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,6 +5,12 @@ class App < Sinatra::Base
     erb :newteam
   end
 
+  post '/newteam' do
+    original_string = params["string"]
+
+    erb :team
+  end
+
   get '/team' do
     # Write your code here!
     @team = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
